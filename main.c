9ca5fea262c7f81243a4dfe610fdd856945d2736
MUKESH KUMAR DHAL <96075190+Mukesh17-cell@users.noreply.github.com>
2021-12-31 12:54:08 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,31 +1,33 @@
-/******************************************************************************
+//
+//  main.c
+//  interest table for investment
+//
+//  Created by MUKESH KUMAR DHAL on 31/12/21.
+//
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
-#include<stdlib.h>
-#include<conio.h>
+#include<math.h>
 
-void  main()
-{
-   int i;
-   char month[12][20]={"JANUARY","FEBRUARY","MARCH","APRIL","MAY","JUNE","JULY",
-                        "AUGUST","SEPTEMBER","OCTOBER", "NOVEMBER","DECEMBER"};
-   
-   printf(" enter the month value\n");
-   scanf(" %d",&i);
-   if(i<1||i>12)
-   {
-   printf(" the value been entered is an error , pls give a valid input\n");
-   getch();
-   }
-     if(i!=12)
-   printf(" %s comes before the month %s", month[i-1],month[i]);
-   else
-   printf(" %s comes before the month %s\n", month[i-1], month[0]);
-   getch();
+int main() {
+    int year ,period;
+    float inrate ,amount , value;
+    printf(" dear user enter the amount that you want to invest\n");
+    scanf(" %f", &amount);
+    printf(" enter the time period for which you want to invest\n");
+    scanf(" %d", &period);
+    printf(" enter the interest rate been offered by the bank\n");
+    scanf(" %f ",&inrate);
+    printf("  AMOUNT THAT IS INVESTED       INRATE        PERIOD \n");
+    printf("         %f                      %f                %d     \n", amount, inrate, period);
+    printf("        value                year \n");
+    year=1;
+    while(year<=period)
+    {
+        value= amount + inrate * amount;
+        amount=value;
+       
+        printf("     %f                                %d \n", value ,  year);
+        year+=1;
+        
+    }
 }
