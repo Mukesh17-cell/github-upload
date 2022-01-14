473e3a81bf90974d41dc5bddd24be12677a84369
MUKESH KUMAR DHAL <96075190+Mukesh17-cell@users.noreply.github.com>
2022-01-14 12:47:09 +0530

    Add files via upload
    this program give the output
    a) total value of sale by each girl
    b) total value of the each item sold
    c) grand total of the items sold

--- main.c
+++ main.c
@@ -1,33 +1,60 @@
 //
 //  main.c
-//  interest table for investment
+//  binomial
 //
-//  Created by MUKESH KUMAR DHAL on 31/12/21.
+//  Created by MUKESH KUMAR DHAL on 10/01/22.
 //
+    
+#include<stdio.h>
+#include <math.h>
+#define MAXGIRLS 5
+#define MAXITEMS 4
 
-#include <stdio.h>
-#include<math.h>
-
-int main() {
-    int year ,period;
-    float inrate ,amount , value;
-    printf(" dear user enter the amount that you want to invest\n");
-    scanf(" %f", &amount);
-    printf(" enter the time period for which you want to invest\n");
-    scanf(" %d", &period);
-    printf(" enter the interest rate been offered by the bank\n");
-    scanf(" %f ",&inrate);
-    printf("  AMOUNT THAT IS INVESTED       INRATE        PERIOD \n");
-    printf("         %f                      %f                %d     \n", amount, inrate, period);
-    printf("        value                year \n");
-    year=1;
-    while(year<=period)
+int main(){
+    int i, j, grandtotal;
+    int value[MAXGIRLS][MAXITEMS] , girltotal[MAXGIRLS], itemtotal[MAXITEMS];
+    
+    /*  READING THE VALUES AND CALCULATING THE GIRLS TOTAL */
+    printf(" INPUT DATA \n");
+    printf(" enter the values one at a time, row wise\n");
+    for(i=0;i< MAXGIRLS;i++)
     {
-        value= amount + inrate * amount;
-        amount=value;
-       
-        printf("     %f                                %d \n", value ,  year);
-        year+=1;
+        girltotal[i]=0;
+        for(j=0;j<MAXITEMS;j++)
+        {
+            scanf(" %d",&value[i][j]);
+            girltotal[i]= girltotal[i] + value[i][j];
+            
+        }
+    }
+    /* COMPUTING THE ITEMS TOTAL */
+    for(j=0; j<MAXITEMS;j++)
+    {
+        itemtotal[j]=0;
+        for(i=0; i<MAXGIRLS;i++)
+        {
+            itemtotal[j]=itemtotal[j]+ value[i][j];
+            
+            
+        }
+    }
+    
+    /*computiing the grand total*/
+    grandtotal=0;
+    for(i=0;i<MAXGIRLS;i++)
+    {
+        grandtotal= grandtotal+ girltotal[i];
         
     }
+    
+    
+    /*printing of the reslts*/
+    printf(" \n GIRLS TOTAL \n");
+    for(i=0;i< MAXGIRLS ;i++)
+        printf(" salesgirl[%d]= %d\n", i+1, girltotal[i]);
+    printf("\n ITEMS TOTAL\n");
+    for(j=0;j<MAXITEMS;j++)
+        printf(" itemsno[%d]= %d\n", j+1, itemtotal[j]);
+    printf(" grand total=  %d", grandtotal);
+    
 }
